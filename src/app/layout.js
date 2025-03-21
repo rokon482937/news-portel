0a99823e89c0c639c559e43499ffc1775c230c6c
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-03-21 22:34:43 +0600

    add code..

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "@/components/shared/footer/Footer";
+import Header from "@/components/shared/header/Header";
+
+import { Newsreader } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const newsreader = Newsreader({
   subsets: ["latin"],
 });
 
@@ -19,10 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={newsreader.className}>
+        <Header></Header>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   );
